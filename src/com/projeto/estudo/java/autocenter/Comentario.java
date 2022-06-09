03f0a868e0ef6fda909d0182e1819b8144a13c69
RFrois <99776659+RFrois@users.noreply.github.com>
2022-06-08 23:31:00 -0300

    atualizando comentário

--- src/com/projeto/estudo/java/autocenter/Comentario.java
+++ src/com/projeto/estudo/java/autocenter/Comentario.java
@@ -3,11 +3,11 @@ package com.projeto.estudo.java.autocenter;
 import java.util.Scanner;
 
 public class Comentario {
-// comentário de uma lunha 
+// comentï¿½rio de uma lunha 
 /*
- * para comentário de multiplas linas 
+ * para comentï¿½rio de multiplas linas 
  */
-/** para comentário docjava 
+/** para comentï¿½rio docjava 
  * @author rafael (ainda em estudo)
  * @uses 
  */
@@ -18,13 +18,14 @@ public class Comentario {
 		 
 		System.out.println("Qual a sua idade?");
 		int idade = teclado.nextInt();
-		System.out.println("Qual o seu nome?");
+		teclado.nextLine();
+		System.out.println("Qual o seu nome?");		
 		String nome = teclado.nextLine();
 		System.out.println("Qual o seu sobrenome?");
 		String sobrenome = teclado.nextLine();
 		System.out.println();
 		 
 		teclado.close();
-		System.out.println("idade: " + idade + "nome: " + nome + "sobrenome: "+ sobrenome);
+		System.out.println("idade: " + " " + idade + "nome: " + nome + "sobrenome: "+ sobrenome);
 	}
 }
